04efd97e784cfdad5ea56fc26bc8ca475cc73499
gusarovFG <gusarovfg@gmail.com>
2024-03-21 01:52:22 +0400

    feat: задание "Спорт это классно!" выполнено

--- task-sport/bin/surf_dart_courses_template.dart
+++ task-sport/bin/surf_dart_courses_template.dart
@@ -1,40 +1,79 @@
-class PokerPlayer {
-  /// Список текущих карт в руке у игрока
-  List<String> _currentHand = ['King of clubs', 'Nine of hearts'];
+void main() {
+  final List<CSGOGamer> gamers = [];
+  final List<Title> titles = [
+    Title('Лучшая команда на Б', 'Лан в подвале'),
+    Title('Лучшая команда на лане', 'Лан в игровом')
+  ];
+  final Team team = Team('ASD', gamers, 'Россия', titles, 234, 144, 90);
+  final CSGOGamer gamer = CSGOGamer(
+      'Фаддей', 'Гусаров', 29, 'Россия', team, 'GusarovFG', 144, 94, 50);
 
-  /// Субъективная оценка выигрыша
-  double _surenessInWin = 0;
-
-  /// Вычислить шансы на выигрыш
-  void calculateProbabilities(
-    List<String> cardOnDesk,
-
-    /// Это часть первого задания. [Strategy] пока не сущестивует.
-    ///
-    /// Опишите его.
-    Strategy strategy,
-  ) =>
-      _surenessInWin = strategy(cardOnDesk, _currentHand);
+  gamer.descriptionOfGamer();
 }
 
-typedef Strategy = double Function(List<String>, List<String>);
+class Person {
+  String name;
+  String secondname;
+  int age;
+  String countryOfBirth;
 
-void main() {
-  final opponent = PokerPlayer();
-
-  /// Это часть первого задания. [Strategy] пока не сущестивует.
-  ///
-  /// Опишите его.
-  final Strategy fakeStrategy = (p0, p1) {
-    print('Стол:\n${p0.reduce((value, element) => '$value, $element')}');
-    print('Оппонент:\n${p1.reduce((value, element) => '$value, $element')}');
-    print('Шансы на победу - 0.31');
-
-    return 0.31;
-  };
-
-  opponent.calculateProbabilities(
-    ['Nine of diamonds', 'king of hearts'],
-    fakeStrategy,
+  Person(
+    this.name,
+    this.secondname,
+    this.age,
+    this.countryOfBirth,
   );
+}
+
+class CSGOGamer extends Person {
+  Team team;
+  String nickName;
+  int countOfgames;
+  int wins;
+  int defeats;
+  int get percentOfWins => (wins / (countOfgames / 100)).round();
+
+  CSGOGamer(
+    super.name,
+    super.secondname,
+    super.age,
+    super.countryOfBirth,
+    this.team,
+    this.nickName,
+    this.countOfgames,
+    this.wins,
+    this.defeats,
+  );
+
+  void descriptionOfGamer() {
+    print(
+        'Имя: $name, Фамилия: $secondname, возраст: $age, рожден в $countryOfBirth, никнейм: $nickName, состоит в команде ${team.name}, сыграно матчей: $countOfgames, процент выйгрышей: $percentOfWins %.\nКоманда имеет титулы: ${team.titles.map((e) => e.nameOfTitle).toList().join(', ')}');
+  }
+}
+
+class Team {
+  String name;
+  List<CSGOGamer> commandStructure;
+  String country;
+  List<Title> titles;
+  int countOfGames;
+  int wins;
+  int defeats;
+
+  Team(
+    this.name,
+    this.commandStructure,
+    this.country,
+    this.titles,
+    this.countOfGames,
+    this.wins,
+    this.defeats,
+  );
+}
+
+class Title {
+  String nameOfTitle;
+  String event;
+
+  Title(this.nameOfTitle, this.event);
 }
