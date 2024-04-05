9840d94667441cb2806a9f78803ac26b57b06229
gusarovFG <gusarovfg@gmail.com>
2024-04-05 18:50:57 +0400

    feat: Добавлен шрифт для темы, реализована кнопка "назан", добавлены цвета"

--- shesterochka/shesterochka/AppColors/AppColors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  final Color textColor = Color.fromARGB(255, 37, 40, 73);
-}
--- shesterochka/shesterochka/lib/main.dart
+++ shesterochka/shesterochka/lib/main.dart
@@ -1,32 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:shesterochka/AppColors/AppColors.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final _appColors = AppColors();
+
+  MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'Sora'),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: _appColors.backgroundColor,
+          elevation: 0,
           title: RichText(
             textAlign: TextAlign.center,
-            text: const TextSpan(
+            text: TextSpan(
               text: "Чек № 56",
-              style: TextStyle(fontSize: 18, fontFamily: 'Sora'),
+              style: TextStyle(fontSize: 18, color: _appColors.textColor),
               children: <TextSpan>[
                 TextSpan(
                   text: '\n24.02.23 в 12:23',
                   style: TextStyle(
-                    fontSize: 10,
-                  ),
+                      fontSize: 10, color: _appColors.secondTextColor),
                 ),
               ],
             ),
           ),
+          leading: BackButton(color: _appColors.buttonsColor),
         ),
       ),
     );
